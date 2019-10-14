b31a09c4c98a830008b9c912ad79629eeebaf677
1161709455@qq.com <h8643160@>
2019-10-14 11:48:40 +0800

    demo

---
+++ App/Rpc/Service/UserService.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 19-10-14
+ * Time: 上午10:29
+ */
+
+namespace App\Rpc\Service;
+
+
+use EasySwoole\Rpc\AbstractService;
+
+class UserService extends AbstractService
+{
+
+    public function serviceName(): string
+    {
+        // TODO: Implement serviceName() method.
+        return 'user';
+    }
+
+    public function register() {
+        var_dump('register');
+        $this->response()->setResult('register');
+    }
+}
---
+++ test.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 19-10-14
+ * Time: 上午10:34
+ */
+
+require_once 'vendor/autoload.php';
+
+use EasySwoole\Rpc\Config;
+use EasySwoole\Rpc\Rpc;
+use EasySwoole\Rpc\NodeManager\RedisManager;
+use EasySwoole\Rpc\Response;
+use EasySwoole\Rpc\ServiceCall;
+
+//cli模拟，跨进程，重新new rpc对象
+$config = new Config();
+$nodeManager = new RedisManager('127.0.0.1');
+$config->setNodeManager($nodeManager);
+$rpc = new Rpc($config);
+
+go(function () use ($rpc) {
+    $client = $rpc->client();
+    $client->addCall('user', 'register', ['arg1', 'arg2'])
+        ->setOnFail(function (Response $response, ServiceCall $call) {
+            print_r($response->toArray());
+            // var_dump($call);
+        })
+        ->setOnSuccess(function (Response $response, ServiceCall $call) {
+            var_dump(12);
+            print_r($response->toArray());
+            // print_r($call);
+        });
+
+    $client->exec();
+});
